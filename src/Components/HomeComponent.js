ee2dd3b540d4b70f2bb4499454fa78b874aff0ff
Matthew Jovenal <matthewjovenal1@outlook.com>
2022-01-03 23:36:14 -0700

    collection row made dynamically - row responsive

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,7 +1,20 @@
-import React from "react";
-import { Button } from "bootstrap";
+import React, {useState} from "react";
+import { COFFEECOLLECTION } from "../shared/coffeeCollection";
+
+const RenderCoffee = ({coffee}) => {
+    return(
+        <div className=" col-md-3">
+            <img src={coffee.image} className=" img-fluid" /><br></br><br></br>
+            <h2 className=" text-center">{coffee.title}</h2>
+            <p className="text-center">{coffee.description}</p>
+        </div>
+    );
+}
 
 const Home = () => {
+
+    const [coffeeCollection] = useState(COFFEECOLLECTION);
+
     return(
         <div className="container">
             <div className="card text-white border-0" style={{'padding-bottom':'1%'}} >
@@ -9,7 +22,18 @@ const Home = () => {
                 <div className="card-img-overlay">
                     <h1 className="card-title display-1 home-jumbotron-header">Great coffee made simple.</h1>
                     <p className="card-text home-jumbotron-text">Start your mornings with the world's best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.</p>
-                    <button className="btn btn-info text-white">Create your plan</button>
+                    <a className=" btn btn-info text-white ">Create your plan</a>
+                </div>
+            </div>
+            <div className="row collection-spacing">
+                <div className="col">
+                    <h1 className="text-center display-1">our collection</h1>
+                    <div className="row">
+                        <RenderCoffee coffee={coffeeCollection[0]} />
+                        <RenderCoffee coffee={coffeeCollection[1]} />
+                        <RenderCoffee coffee={coffeeCollection[2]} />
+                        <RenderCoffee coffee={coffeeCollection[3]} />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/shared/coffeeCollection.js
@@ -0,0 +1,30 @@
+export const COFFEECOLLECTION = [
+    
+    {
+        id: 0,
+        title: 'Gran Espresso',
+        description: 'Light and flavorful blend with cocoa and black pepper for an intense experience.',
+        image: '/assets/home/desktop/image-gran-espresso.png'
+    },
+
+    {
+        id: 1,
+        title: 'Planalto',
+        description: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts.',
+        image: '/assets/home/desktop/image-planalto.png'
+    },
+
+    {
+        id: 2,
+        title: 'Piccollo',
+        description: 'Mild and smooth blend featuring notes of toasted almond and dried cherry.',
+        image: '/assets/home/desktop/image-piccollo.png'
+    },
+
+    {
+        id: 3,
+        title: 'Danche',
+        description: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes.',
+        image: '/assets/home/desktop/image-danche.png'
+    }
+]
